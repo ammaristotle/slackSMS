9f8398f4f05a1b24af07e61e33a76983e7eb2d27
ammar <ammar.d.khan@gmail.com>
2016-09-19 19:55:24 -0400

    Adds basic slack functionality test

--- test/slack.spec.js
+++ test/slack.spec.js
@@ -1,11 +1,19 @@
-const expect = require("chai").expect;
-const request = require('request');
-const url = "http://localhost:4000/api/slack"
-const mock_data = {};
+var request = require('request');
+var chai = require('chai');
+var expect = chai.expect;
 
-describe('Slack to Twilio', function() {
-  it('Can hit the API endpoint', function() {
-    // finish
+describe('Slack Interface', function() {
+  it('can hit the url', function(done) {
+    var url = 'http://localhost:4000/api/slack';
+    var mock_request = {
+      form: {
+        From: '+5555555555',
+        Body: 'Hey there, testing if this works!'
+      }
     }
+    request.post(url, mock_request, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    })
   })
-})
+});
