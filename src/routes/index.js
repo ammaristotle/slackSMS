5f35db1c4be9e933c40db3508cd3853fbaf8d2d8
ammar <ammar.khand@gmail.com>
2017-03-08 18:08:21 -0500

    Patches bug with routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const slackAPI = require('../models/slack');
 const twilioAPI = require('../models/twilio');
 
-router.post('/slack', slackAPI.sendToSlack);
-router.post('/twilio', twilioAPI.sendToTwilio);
+router.post('/twilio', slackAPI.sendToSlack);
+router.post('/slack', twilioAPI.sendToTwilio);
 
 module.exports = router;
